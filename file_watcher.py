8cef25d1ca00be3993a09e09ec1a7eb446a88038
BuildTools <unconfigured@null.spigotmc.org>
2022-05-12 23:23:50 -0300

    removido métodos redundantes

--- file_watcher.py
+++ file_watcher.py
@@ -3,23 +3,14 @@ from watchdog.observers import Observer
 
 
 def start(nlg_class):
-    def on_created(event):
-        nlg_class.load_domain()
-
-    def on_deleted(event):
-        nlg_class.load_domain()
-
-    def on_modified(event):
-        nlg_class.load_domain()
-
-    def on_moved(event):
+    def load_domain(event):
         nlg_class.load_domain()
 
     my_event_handler = PatternMatchingEventHandler(["*.yml"])
-    my_event_handler.on_created = on_created
-    my_event_handler.on_deleted = on_deleted
-    my_event_handler.on_modified = on_modified
-    my_event_handler.on_moved = on_moved
+    my_event_handler.on_created = load_domain
+    my_event_handler.on_deleted = load_domain
+    my_event_handler.on_modified = load_domain
+    my_event_handler.on_moved = load_domain
     my_observer = Observer()
     my_observer.schedule(my_event_handler, nlg_class.domain_path, recursive=True)
     my_observer.start()
